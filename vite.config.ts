ff1405570dcaf43fc6096f7e088c3db19c6b72de
Ron Tseytlin <ronts390@gmail.com>
2023-04-18 23:45:23 +0300

    changed proxy address to 127.0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3333',
+        target: 'http://127.0.0.1:3333',
         changeOrigin: true
       },
     }
